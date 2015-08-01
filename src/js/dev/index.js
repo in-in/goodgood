c1b329d8d87b06b42830a70cf6870f78b8894650
in-in <timothybernerslee@gmail.com>
2015-08-02 00:57:12 +0300

    update

--- src/js/dev/index.js
+++ src/js/dev/index.js
@@ -1,6 +1,7 @@
-//var elem = document.querySelector('.slider');
-//var flkty = new Flickity( elem, {
-//  // options
-//  cellAlign: 'center',
-//  contain: true
-//});
+var elem = document.querySelector('.slider');
+var flkty = new Flickity( elem, {
+  cellAlign: 'center',
+  contain: true,
+  prevNextButtons: false,
+  lazyLoad: true
+});
