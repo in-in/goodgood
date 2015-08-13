54ec7695128ba16a82a8e0065249b0e1abe014a3
in-in <timothybernerslee@gmail.com>
2015-08-14 00:27:36 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -5277,8 +5277,33 @@ var flkty = new Flickity( elem, {
   initialIndex: 3
 });
 
-var element = document.querySelector('.reviews-slider');
-var flkty = new Flickity( element, {
-  cellAlign: 'center',
-  prevNextButtons: false,
+var utils = window.fizzyUIUtils;
+
+docReady( function() {
+  var gallery = document.querySelector('.reviews-slider');
+  var flkty = new Flickity( gallery, {
+    prevNextButtons: false,
+    pageDots: false,
+    initialIndex: 1
+  });
+  // elements
+  var cellsButtonGroup = document.querySelector('.persons');
+  var cellsButtons = utils.makeArray( cellsButtonGroup.children );
+
+  // update buttons on select
+  flkty.on( 'cellSelect', function() {
+    var previousSelectedButton = cellsButtonGroup.querySelector('.person__current');
+    var selectedButton = cellsButtonGroup.children[ flkty.selectedIndex ];
+    classie.remove( previousSelectedButton, 'person__current' );
+    classie.add( selectedButton, 'person__current' );
+  });
+
+  // cell select
+  eventie.bind( cellsButtonGroup, 'click', function( event ) {
+    if ( !matchesSelector( event.target, '.person' ) ) {
+      return;
+    }
+    var index = utils.indexOf( cellsButtons, event.target );
+    flkty.select( index );
+  });
 });
